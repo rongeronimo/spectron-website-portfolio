8cba6df17c0964e618272aefef94d67e05acb603
Ron Cedric Geronimo <grb1544@dlsud.edu.ph>
2025-10-24 21:30:08 +0800

    edited for ktx2 draco store

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
+// 🚨 import this BEFORE anything else
+import './stores/loader-init.js';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App.jsx';
 
-import * as THREE from 'three';
-import { setupLoaders } from './setuploaders.js';
-
-// Initialize and register loaders globally
-const renderer = new THREE.WebGLRenderer();
-setupLoaders(renderer);
-
 createRoot(document.getElementById('root')).render(
   <App />
-    
 );
+
