1c9baeab43fc637d01ef028a2dac159c52c2118c
Ron Cedric Geronimo <grb1544@dlsud.edu.ph>
2025-12-04 13:23:08 +0800

    removed Strict Mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 // 🚨 import this BEFORE anything else
 import './stores/loader-init.js';
 
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App.jsx';
