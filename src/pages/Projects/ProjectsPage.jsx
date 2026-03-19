307a00c62632a15c167522fb92f97b04cb41e0d0
Ron Cedric Geronimo <grb1544@dlsud.edu.ph>
2026-03-19 11:41:30 +0800

    added descriptions to each project

--- src/pages/Projects/ProjectsPage.jsx
+++ src/pages/Projects/ProjectsPage.jsx
@@ -32,7 +32,7 @@ const ProjectsPage = () => {
             imageSrc: "/images/EmoWarn.png",
             title: "EmoWarn",
             subheader: "HTML   ·   CSS  ·   Python   ·   Flask",
-            content: ["EmoWarn is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."],
+            content: ["EmoWarn is a light web application that uses a news article's headline and main content to analyze the probability of it being an article composed for the purpose of spreading misinformation, colloquially known as fake news. It also analyzes the probability of which emotion the reader is likely to feel after reading the article to attempt to warn the user about any emotional biases in the article. The emotions are based on Paul Ekman's basic emotions of Joy, Anger, Fear, Sadness, and Surprise."],
             buttons: [
               {
                 label: "Website",
@@ -48,7 +48,7 @@ const ProjectsPage = () => {
             imageSrc: "/images/FlowKanban.png",
             title: "Flow",
             subheader: "HTML   ·   Javascript   ·   CSS",
-            content: ["Flow is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."],
+            content: ["Flow is a simple kanban board application to organize your to-dos and track your progress. With flow, you can add, delete, and update your to-dos and categorize them between to-do, ongoing, and done by moving them across each provided column."],
             buttons: [
               {
                 label: "Website",
@@ -64,7 +64,7 @@ const ProjectsPage = () => {
             imageSrc: "/images/Cifake.png",
             title: "AI Image Detector",
             subheader: "Python   ·   Tensorflow   ·   OpenCV",
-            content: ["This prototype is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."],
+            content: ["This project developed an attention-based convolutional neural network (CNN) to classify real versus AI-generated synthetic images using the CIFAKE dataset. The model learns to focus on subtle visual artifacts indicative of synthetic image generation, improving classification performance."],
             buttons: [
               {
                 label: "GitHub",
@@ -76,7 +76,7 @@ const ProjectsPage = () => {
             imageSrc: "/images/RegexWizard.png",
             title: "RegEx Wizard",
             subheader: "HTML   ·   Javascript   ·   CSS   ·   Python",
-            content: ["This prototype is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."],
+            content: ["RegEx Wizard is an interactive educational website/tool for validating strings using Regular Expressions and simulating Deterministic Finite Automata (DFA). It visualizes state transitions to explain why a string is accepted or rejected and can generate corresponding Context-Free Grammars (CFG) and Pushdown Automata (PDA) structures. "],
             buttons: [
               {
                 label: "GitHub",
@@ -88,7 +88,7 @@ const ProjectsPage = () => {
             imageSrc: "/images/Emoculus.png",
             title: "Emoculus",
             subheader: "Python   ·   DeepFace  ·   OpenCV   ·   Dlib",
-            content: ["This prototype is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."],
+            content: ["This prototype is a real-time driver emotion recognition system designed to improve road safety by detecting emotionally compromised driving states such as stress, anger, fatigue, and drowsiness. The system analyzes live facial expressions using computer vision and deep learning techniques to identify risky emotional conditions that may affect driving performance."],
             buttons: [
               {
                 label: "GitHub",
