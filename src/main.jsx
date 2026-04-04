c95d35ad691889b8ad2e7384062129ba7bf48e77
Ron Cedric Geronimo <grb1544@dlsud.edu.ph>
2026-04-04 23:08:51 +0800

    router changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router';
-import AboutPage from './pages/About/AboutPage.jsx';
-import SkillsPage from './pages/Skills/SkillsPage.jsx';
-import ProjectsPage from './pages/Projects/ProjectsPage.jsx';
-import ContactsPage from './pages/Contacts/ContactsPage.jsx';
-import HomePage from './pages/Home/HomePage.jsx';
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<HomePage />} />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="skills" element={<SkillsPage />} />
-        <Route path="project-experience" element={<ProjectsPage />} />
-        <Route path="contacts" element={<ContactsPage />} />
-      </Route>
-    </Routes>
+      <App />
   </BrowserRouter>
 );
 
