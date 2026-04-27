7588bfcc533996ed384dfa8154f18ae3484f2115
Ron Cedric Geronimo <grb1544@dlsud.edu.ph>
2026-04-27 17:06:16 +0800

    edited about infos

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -12,14 +12,14 @@ const AboutPage = () => {
               imageSrc: "/images/About.png",
               title: "Introduction",
               header: "Ron Cedric B. Geronimo",
-              subheader: "Frontend Developer / 3D Web Developer",
+              subheader: "Frontend Web Developer / 3D Web Developer",
               content: [
                 "Bachelor of Science in Computer Science | Intelligent Systems",
                 "De La Salle University - Dasmariñas | 2021-2025 ",
-                "I am an aspiring Frontend Developer focused on creating immersive 3D web experiences using modern frontend technologies. I strive to build responsive, interactive interfaces that prioritize performance and user experience. My interest in frontend development has grown because it allows me to see my work and outcomes more immediately and directly than other programming languages. I work with my projects using React, Three.js, HTML, CSS, JavaScript, GSAP, and other frontend technologies. Moreover, I have proficiency in backend development, artificial intelligence, and data science, with experience in Python and PHP. Currently, I am seeking opportunities to further enhance my skills and contribute to projects that push the boundaries of interactive web design. Similarly, I am continuously working towards becoming a developer who values clean code, intuitive design, and meaningful user experiences."
+                "I am an aspiring Frontend Web Developer focused on creating immersive 3D web experiences using modern frontend technologies. I strive to build responsive, interactive interfaces that prioritize performance and user experience. My interest in frontend web development has grown because it allows me to see my work and outcomes more immediately and directly than other programming languages. I work with my projects using React, Three.js, HTML, CSS, JavaScript, GSAP, and other frontend technologies. Moreover, I have proficiency in backend development, artificial intelligence, and data science, with experience in Python and PHP. Currently, I am seeking opportunities to further enhance my skills and contribute to projects that push the boundaries of interactive web design. Similarly, I am continuously working towards becoming a developer who values clean code, intuitive design, and meaningful user experiences."
               ],
               variant: "profile",
-              resumeHref: "/src/data/GeronimoRon_JobResume.pdf",
+              resumeHref: "/src/data/Geronimo_Resume.pdf",
             },
             {
               imageSrc: "/images/About2.png",
@@ -30,8 +30,8 @@ const AboutPage = () => {
             {
               imageSrc: "/images/About3.png",
               content: [
-                "I am exploring 3D web development as a way to build immersive web experiences that blend interactivity with visual storytelling. I aim to design interfaces that go beyond traditional layouts, offering users more engaging and memorable interactions. It is advantageous to use 3D because it brings depth into the browser which includes rotations, zooms, and animations that make static pages dynamic.",
-                "I am particularly inspired by games, cinematic environments, and story-based design, where each element contributes to a larger experience. I combine design and development by creating 3D assets in Blender and integrating them into the web using modern frontend technologies. Through this approach, users feel curious, immersed, and connected to the interface in a more meaningful way. While I have a strong interest in 3D, I remain equally capable of developing clean and responsive traditional interfaces, adapting my approach based on project needs."
+                "I am exploring 3D web development as a way to build immersive web experiences that blend interactivity with visual storytelling. I aim to design interfaces that go beyond traditional layouts, offering users more engaging and memorable interactions. The use of 3D introduces depth into the browser which includes rotations, zooms, and animations that transform static pages dynamically.",
+                "I am particularly inspired by games, cinematic environments, and story-based design, where each element contributes to a larger experience. I combine design and development by creating 3D assets in Blender and integrating them into the web using modern frontend technologies. Through this approach, users feel curious, immersed, and connected to the interface in a more meaningful way. While I have a strong interest in 3D, I remain equally capable of developing clean and responsive traditional interfaces, which adapts my approach based on project needs."
               ],
             },
           ]}
